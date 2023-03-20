4aceacb3b82c89f70d6cdbab66f8b5dddfad53f2
Kacper Ledwon <you@example.com>
2023-03-20 17:33:57 +0100

    add .env, import TypeOrmModule and ConfigModule to app module

--- skill-swap-be/src/app.module.ts
+++ skill-swap-be/src/app.module.ts
@@ -6,9 +6,32 @@ import { AuthModule } from './auth/auth.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { SkillsModule } from './skills/skills.module';
 import { ChatModule } from './chat/chat.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AuthModule, ProfilesModule, SkillsModule, ChatModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    AuthModule,
+    ProfilesModule,
+    SkillsModule,
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
