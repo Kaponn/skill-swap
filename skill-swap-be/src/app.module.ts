386bb785536858ed3af0b2f9b0457a706155844e
Kacper Ledwon <you@example.com>
2023-03-20 16:35:07 +0100

    create project structure, generate modules, controllers and services using nest cli

--- skill-swap-be/src/app.module.ts
+++ skill-swap-be/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { SkillsModule } from './skills/skills.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, ProfilesModule, SkillsModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
