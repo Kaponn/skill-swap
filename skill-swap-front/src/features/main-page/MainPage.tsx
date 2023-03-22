2bfe889e3ceb033993f023c9863de19ec82359cb
Kacper Ledwon <you@example.com>
2023-03-22 02:14:11 +0100

    create Profile component

--- skill-swap-front/src/features/main-page/MainPage.tsx
+++ skill-swap-front/src/features/main-page/MainPage.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import './styles/MainPage.css';
 import { LogoButton } from '../../components/logo-button/LogoButton';
 import { SearchInput } from '../../components/search-input/SearchInput';
+import { Profile } from './components/Profile';
 
 export const MainPage = () => {
   return(
     <div className="main-page-container">
+      <div className="main-page-header">        
       <LogoButton />
       <SearchInput />
+      </div>
+      <div className="main-page-profiles">
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      <Profile name="Ania" city="Kraków" skills={["playing the guitar", "English"]} wantedSkills={["playing the piano", "Spanish"]} />
+      </div>
     </div>
   )
 }
