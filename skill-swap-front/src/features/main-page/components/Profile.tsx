a4c33e96cf79b81c018c2938057d7554ec57c989
Kacper Ledwon <you@example.com>
2023-03-22 02:14:11 +0100

    create Profile component

--- skill-swap-front/src/features/main-page/components/Profile.tsx
+++ skill-swap-front/src/features/main-page/components/Profile.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import '../styles/Profile.css';
 
-export const Profile = () => {
+interface ProfileData {
+  name: string;
+  city: string;
+  skills: string[];
+  wantedSkills: string[];
+}
+
+export const Profile = (props: ProfileData) => {
   return(
     <div className="profile-container">
-
+      <h3 className="profile-name">{props.name}</h3>
+      <h4 className="profile-city">{props.city}</h4>
+      <p className="profile-skills">Skills: {props.skills.map((skill) => (
+        <li className="skills-list">{skill}</li>
+      ))}</p>
+      <p className="profile-skills">Wants: {props.wantedSkills.map((skill) => (
+        <li className="skills-list">{skill}</li>
+      ))}</p>
+      <button className="chat-button">Start Chatting</button>
     </div>
   )
 }
