29ad66fba685d8d90928dc65f9b28e825e8c691f
Kacper Ledwon <you@example.com>
2023-03-22 15:31:29 +0100

    create HamburgerMenu component

---
+++ skill-swap-front/src/components/hamburger-menu/HamburgerMenu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/HamburgerMenu.css';
+import logo from '../../assets/hamburger-menu-24.png';
+
+export const HamburgerMenu = () => {
+  return(
+    <button className="menu-button">
+      <img src={logo} alt="menu"/>
+    </button>
+  )
+}
