8c8146a703e0962d4f834a91d8de3398747a4ab6
Kacper Ledwon <you@example.com>
2023-04-12 16:09:17 +0200

    create styles for ProfilePage component

--- skill-swap-front/src/features/profile-page/ProfilePage.tsx
+++ skill-swap-front/src/features/profile-page/ProfilePage.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useState } from "react";
 import { CgProfile } from "react-icons/cg";
-import { Menu } from "../menu/Menu"
+import { Menu } from "../menu/Menu";
+import "./styles/ProfilePage.css";
 
 interface UserDetails {
   cityName: string,
@@ -21,43 +22,45 @@ export const ProfilePage = (user: UserDetails) => {
   return (
     <div className="global-container">
       <Menu />
+        <div className="profile-icon-container">
+          <CgProfile className="profile-icon" />
+        </div>
       <div className="profile-container">
-        <CgProfile className="profile-icon name" />
-        <div className="profile-left-side">
+        <div className="content-container">
           <div className="detail-container">
             <p className="profile-paragraph">Location</p>
-            <input className="profile-input" value={cityName} onChange={(e) => setCityName(e.target.value)} />
+            <input className="profile-input city" value={cityName} onChange={(e) => setCityName(e.target.value)} />
           </div>
           <div className="detail-container">
             <p className="profile-paragraph">Date Of Birth</p>
             <div className="date-of-birth-container">
-              <p>{user.dateOfBirth.getDay()}</p>
-              <p>{user.dateOfBirth.getMonth()}</p>
+              <p>{user.dateOfBirth.getDay()}.</p>
+              <p>{user.dateOfBirth.getMonth()}.</p>
               <p>{user.dateOfBirth.getFullYear()}</p>
             </div>
           </div>
           <div className="detail-container">
             <p className="profile-paragraph">Email</p>
-            <p className="profile-paragraph">{user.email}</p>
+            <p className="email-paragraph">{user.email}</p>
           </div>
-          <div className="profile-buttons">
+          <div className="profile-buttons-container">
             <button className="profile-button">Change Email</button>
             <button className="profile-button">Change Password</button>
           </div>
         </div>
-        <div className="profile-right-side">
+        <div className="content-container">
           <div className="detail-container">
             <p className="profile-paragraph">Skills</p>
-            <textarea className="skills input" value={skills} onChange={(e) => setSkills(e.target.value)} />
+            <textarea className="profile-input box" value={skills} onChange={(e) => setSkills(e.target.value)} />
           </div>
           <div className="detail-container">
             <p className="profile-paragraph">Wanted Skills</p>
-            <textarea className="skills input" value={wantedSkills} onChange={(e) => setWantedSkills(e.target.value)} />
+            <textarea className="profile-input box" value={wantedSkills} onChange={(e) => setWantedSkills(e.target.value)} />
           </div>
         </div>
-        <div className="bio-container">
-          <p className="detail-container">Bio</p>
-          <textarea className="bio input" value={bio} onChange={(e) => setBio(e.target.value)}/>
+        <div className="content-container">
+          <p className="profile-paragraph">Bio</p>
+          <textarea className="profile-input box bio" value={bio} onChange={(e) => setBio(e.target.value)}/>
         </div>
       </div>
     </div>
